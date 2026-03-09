08b06d9957235269d066d3572ab0a4567dd8ae4c
blob121vr-a11y <blob121vr@outlook.com>
2026-03-08 20:13:51 -0400

    JAVA

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let effect = document.querySelector('button')
-
-effect.onclick = () => {
-    alert('im using a tutorial for this ima need to learn this bro')
-}
+console.log(' wassup');
+console.log('I like ribs!');
+// window.alert('why dont you like ribs?');
+document.getElementById("funtut1");
+let text1 ;
+text1 = "click me";
+//i used a tutorial lol
